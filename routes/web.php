c33152d6ec93e30a4fb453716cb3fee0f6cc5037
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-06-26 22:15:32 +0700

    make route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogCategoryController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\BlogTagController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guide');
+})->name('home.index');
+
+Route::get('/admin', function () {
+    return view('index');
+})->name('dashboard.index');
+
+Route::resource('blogcategories', BlogCategoryController::class);
+Route::resource('blogs', BlogController::class);
+Route::resource('blogtags', BlogTagController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('tags', TagController::class);
