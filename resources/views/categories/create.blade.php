3439d615b84258ef1b92eb70963240e7e6c80452
Muhammad Taufik Hidayat <muhammadtaufik@webmail.umm.ac.id>
2021-06-28 22:08:58 +0800

    create categories

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.master')
+@section('title', 'Create Blogs')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Categories</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('blogs.index') }}">Blogs</a></li>
+                        <li class="breadcrumb-item active">Create</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Create Data</h3>
+                </div>
+                <!-- /.card-header -->
+                <form action="{{ route('blogs.store') }}" method="POST" enctype="multipart/form-data">
+                    @CSRF
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="title">Category Name</label>
+                                    <input type="text" name="categoryName" class="form-control @error('title') is-invalid @enderror" placeholder="Category Name">
+                                    <small class="text-danger">@error('categoryName') {{$message}} @enderror</small>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <label for="featuredImage">Icon Image</label>
+                                    <input type="file" name="iconImage" class="form-control @error('featuredImage') is-invalid @enderror" placeholder="Icon Image">
+                                    <small class="text-danger">@error('iconImage') {{$message}} @enderror</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-end">
+                            <a href="{{ route('blogs.index') }}" class="m-1 btn btn-outline-danger">Back</a>
+                            <button type="submit" class="m-1 btn btn-success">Create</button>
+                        </div>
+                    </div>
+                </form>
+            </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
