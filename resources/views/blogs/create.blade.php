a71d6e82e8163b564f5cac3e3bf156ea15d51b91
farkhan <farhanhamzah71@webmail.umm.ac.id>
2021-07-03 08:08:16 +0700

    change admin adnimation

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -33,42 +33,42 @@
                     @CSRF
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="title">Title</label>
                                     <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" placeholder="Blog Title">
                                     <small class="text-danger">@error('title') {{$message}} @enderror</small>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="post">Post</label>
-                                    <input type="text" name="post" class="form-control @error('post') is-invalid @enderror" placeholder="Blog Post">
+                                    <textarea name="post" class="form-control @error('post') is-invalid @enderror" placeholder="Blog Post"></textarea>
                                     <small class="text-danger">@error('post') {{$message}} @enderror</small>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="post_excerpt">Post Exceprt</label>
                                     <input type="text" name="post_excerpt" class="form-control @error('post_excerpt') is-invalid @enderror" placeholder="Blog Post Excerpt">
                                     <small class="text-danger">@error('post_excerpt') {{$message}} @enderror</small>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="slug">Slug</label>
                                     <input type="text" name="slug" class="form-control @error('slug') is-invalid @enderror" placeholder="Blog Slug">
                                     <small class="text-danger">@error('slug') {{$message}} @enderror</small>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="featuredImage">Featured Image</label>
                                     <input type="file" name="featuredImage" class="form-control @error('featuredImage') is-invalid @enderror" placeholder="Blog Featured Image">
                                     <small class="text-danger">@error('featuredImage') {{$message}} @enderror</small>
                                 </div>
                             </div>
-                            <div class="col-6">
+                            <div class="col-12">
                                 <div class="form-group">
                                     <label for="metaDescription">Meta Description</label>
                                     <input type="text" name="metaDescription" class="form-control @error('metaDescription') is-invalid @enderror" placeholder="Blog Meta Description">
